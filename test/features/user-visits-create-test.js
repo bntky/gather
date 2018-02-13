a3aa5a256c15119e1565d7975ea405973b323a9a
Brendan Kirby <bkirby@bntky.com>
2018-02-13 13:48:18 -0800

    Gather Part IV step 4 -- Write a feature test to test delete

--- test/features/user-visits-create-test.js
+++ test/features/user-visits-create-test.js
@@ -15,5 +15,19 @@ describe('User visits create page', () => {
       assert.include(browser.getText('body'), title);
       assert.include(browser.getAttribute('.item-img', 'src'), imageUrl);
     });
+
+    it('and then deletes the item', () => {
+      const {title, description, imageUrl} = buildItemObject();
+      browser.url('/items/create');
+
+      browser.setValue('#title-input', title);
+      browser.setValue('#description-input', description);
+      browser.setValue('#imageUrl-input', imageUrl);
+      browser.click('#submit-button');
+      browser.click('.delete-form');
+
+      assert.notInclude(browser.getText('body'), title);
+      assert.notInclude(browser.getAttribute('.item-img', 'src'), imageUrl);
+    });
   });
 });
