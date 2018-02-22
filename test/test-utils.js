91b9ce0fa63e47a69ffdb513c9e83f5736abc659
Brendan Kirby <bkirby@bntky.com>
2018-02-22 12:16:56 -0800

    Make the test color output more ledgible with dark background
    Bright black on black is not visible at all.  Shouldn't this be
    configurable outside the project?

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,5 +1,12 @@
 const {jsdom} = require('jsdom');
 
+var colors = require('mocha/lib/reporters/base').colors;
+colors['pass'] = '97';
+colors['fast'] = '97';
+colors['light'] = '97';
+colors['diff gutter'] = '97';
+colors['error stack'] = '97';
+
 const Item = require('../models/item');
 
 // Create and return a sample Item object
