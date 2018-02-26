0a070c77b905020df14a102481e97e61e1e7ed9b
Brendan Kirby <bkirby@bntky.com>
2018-02-25 20:22:22 -0800

    Feature test failing going to server level to fix it

--- test/features/user-visits-undo-delete-test.js
+++ test/features/user-visits-undo-delete-test.js
@@ -19,6 +19,24 @@ describe('User visits root', () => {
 
       assert.include(browser.getText('body'), 'Undo last delete');
     });
+
+    it('undeletes deleted item', function() {
+      this.retries(3);
+      const {title, description, imageUrl} = buildItemObject();
+      browser.url('/items/create');
+      browser.setValue('#title-input', title);
+      browser.setValue('#description-input', description);
+      browser.setValue('#imageUrl-input', imageUrl);
+      browser.click('#submit-button');
+      browser.execute(function () {
+        document.querySelectorAll('.delete-form')[0].submit();
+      });
+
+      browser.click('#undo-button');
+
+      assert.include(browser.getText('body'), title);
+      assert.include(browser.getAttribute('.item-img', 'src'), imageUrl);
+    });
   });
 });
 
